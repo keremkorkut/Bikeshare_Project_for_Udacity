0e01857a71af48b88cab773b3f01da8ad2f1575a
keremkorkut <keremkorkut@gmail.com>
2020-06-25 17:54:21 +0300

    extra infos added

--- bikeshare_kerem_rev1.py
+++ bikeshare_kerem_rev1.py
@@ -15,9 +15,9 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data!,with together')
     
-    print('My name is Kerem :)')
+    print('My name is Kerem Korkut:)')
 
         # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
